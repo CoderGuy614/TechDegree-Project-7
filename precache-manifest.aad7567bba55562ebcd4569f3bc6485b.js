1ecf6d89f250b0739edbd881666082fdbd6aa904
CoderGuy614 <jlutz.110@gmail.com>
2020-01-09 10:22:40 +0700

    Updates

---
+++ precache-manifest.aad7567bba55562ebcd4569f3bc6485b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fba87a1245ced8f7f29d83288e2f163",
+    "url": "/TechDegree-Project-7/index.html"
+  },
+  {
+    "revision": "407d3829bc2429d27eb9",
+    "url": "/TechDegree-Project-7/static/css/main.7417da49.chunk.css"
+  },
+  {
+    "revision": "1d005fc595277b805af0",
+    "url": "/TechDegree-Project-7/static/js/2.191e0167.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/TechDegree-Project-7/static/js/2.191e0167.chunk.js.LICENSE"
+  },
+  {
+    "revision": "407d3829bc2429d27eb9",
+    "url": "/TechDegree-Project-7/static/js/main.b8c9fe24.chunk.js"
+  },
+  {
+    "revision": "2873d70f3d7c740f4317",
+    "url": "/TechDegree-Project-7/static/js/runtime-main.967247b4.js"
+  }
+]);
--- src/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// This page is rendered if no search results are found.
-const NotFound = props => (
-  <li className='not-found'>
-    <h1>Sorry, no Photos match your search.</h1>
-  </li>
-);
-
-export default NotFound;
